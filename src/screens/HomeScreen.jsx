34be81b0122500b7b82db69905a35d77bad6cd80
jedrzejwyzgala <jedrzejwyzgala@gmail.com>
2023-03-18 15:14:18 +0100

    Configure simple navigation.

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+
+export const HomeScreen = () => (
+  <View style={styles.container}>
+    <Text style={styles.message}>Home screen</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  message: {
+    color: '#000',
+  },
+});
