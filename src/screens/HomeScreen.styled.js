d09566f750e226566564d8a5318b1cba00bebe79
jedrzejwyzgala <jedrzejwyzgala@gmail.com>
2023-03-25 22:26:50 +0100

    Update Home screen to use theme.

---
+++ src/screens/HomeScreen.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  background-color: ${(props) => props.theme.colors.backgroundPrimary};
+  flex: 1;
+  padding-horizontal: 10px;
+`
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,10 @@
-import { FlatList, StyleSheet, Text, View, ListRenderItem } from 'react-native'
+import { FlatList, ListRenderItem } from 'react-native'
 import * as React from 'react'
 import { useSelector } from 'react-redux'
 import { selectAllTodos } from '../store/selectors/todosSelectors'
 import { Todo } from '../types/Todo'
 import { TodoListItem } from '../components/todoListItem/TodoListItem'
+import { Container } from './HomeScreen.styled'
 
 export const HomeScreen = () => {
   const todos = useSelector(selectAllTodos)
@@ -13,20 +14,8 @@ export const HomeScreen = () => {
   )
 
   return (
-    <View style={styles.container}>
-      <Text>Todo List</Text>
+    <Container>
       <FlatList data={todos} renderItem={renderTodo} />
-    </View>
+    </Container>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  message: {
-    color: '#000',
-  },
-})
