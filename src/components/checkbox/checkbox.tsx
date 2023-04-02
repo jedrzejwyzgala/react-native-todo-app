bf31e3362bf46c448208f34ebf2b7257c774cf63
jedrzejwyzgala <jedrzejwyzgala@gmail.com>
2023-04-02 16:50:34 +0200

    Update checkbox styles.

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -8,19 +8,7 @@ export const CheckBox = ({
   size,
   ...rnCheckboxProps
 }: CheckBoxProps & RNCheckBoxProps) => {
-  const theme = useTheme()
   return (
-    <CheckBoxStyled
-      animationDuration={0.2}
-      tintColor={theme.colors.textPrimary}
-      onTintColor={theme.colors.successPrimary}
-      onCheckColor={theme.colors.successPrimary}
-      tintColors={{
-        true: theme.colors.successPrimary,
-        false: theme.colors.textPrimary,
-      }}
-      size={size}
-      {...rnCheckboxProps}
-    />
+    <CheckBoxStyled animationDuration={0.2} size={size} {...rnCheckboxProps} />
   )
 }
