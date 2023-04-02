e9ffdb809498e80139c2595c81765e67c093c9e6
jedrzejwyzgala <jedrzejwyzgala@gmail.com>
2023-04-02 17:51:04 +0200

    Update checkbox styles.

--- src/components/checkbox/checkbox.styled.ts
+++ src/components/checkbox/checkbox.styled.ts
@@ -2,19 +2,7 @@ import RNCheckBox from '@react-native-community/checkbox'
 import styled from 'styled-components/native'
 import { CheckBoxProps } from './checkbox.types'
 
-export const CheckBoxStyled = styled(RNCheckBox).attrs((props) => ({
-  tintColor: props.disabled
-    ? props.theme.colors.textSecondary
-    : props.theme.colors.textPrimary,
-  onTintColor: props.theme.colors.successPrimary,
-  onCheckColor: props.theme.colors.successPrimary,
-  tintColors: {
-    true: props.theme.colors.successPrimary,
-    false: props.disabled
-      ? props.theme.colors.textSecondary
-      : props.theme.colors.textPrimary,
-  },
-}))<CheckBoxProps>`
+export const CheckBoxStyled = styled(RNCheckBox)<CheckBoxProps>`
   width: ${(props) => props.size};
   height: ${(props) => props.size};
   margin: 1px;
--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -6,9 +6,29 @@ import { CheckBoxProps } from './checkbox.types'
 
 export const CheckBox = ({
   size,
+  disabled,
   ...rnCheckboxProps
 }: CheckBoxProps & RNCheckBoxProps) => {
+  const theme = useTheme()
+
+  const uncheckedColor = disabled
+    ? theme.colors.textSecondary
+    : theme.colors.textPrimary
+
+  const checkedColor = theme.colors.successPrimary
+
   return (
-    <CheckBoxStyled animationDuration={0.2} size={size} {...rnCheckboxProps} />
+    <CheckBoxStyled
+      animationDuration={0.2}
+      tintColor={uncheckedColor}
+      onTintColor={checkedColor}
+      onCheckColor={checkedColor}
+      tintColors={{
+        true: checkedColor,
+        false: uncheckedColor,
+      }}
+      size={size}
+      {...rnCheckboxProps}
+    />
   )
 }
